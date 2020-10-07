1c2f057786fe44e869f6468a1fa5d06fe0b9f5af
Art-ss72 <72525253+Art-ss72@users.noreply.github.com>
2020-10-07 16:33:01 -0500

    Agregué mi nombre

--- Holamundo2.c
+++ Holamundo2.c
@@ -3,5 +3,6 @@ int main(){
   printf(“Este es mi segundo codigo”);
   printf(“Programar es facil”);
   printf(“Voy a compartir con mi compañero el codigo”);
+  printf(“\nArturo Salcedo Salas\n”);
 return 0;
 }
