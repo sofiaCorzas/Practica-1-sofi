24723554df485f1b8bea7875a43f4ee9e5438a72
MauricioHernandez2407 <72525262+MauricioHernandez2407@users.noreply.github.com>
2020-10-07 16:27:43 -0500

    Agregue mi nombre en un printf

--- Holamundo2.c
+++ Holamundo2.c
@@ -3,5 +3,6 @@ int main(){
   printf(“Este es mi segundo codigo”);
   printf(“Programar es facil”);
   printf(“Voy a compartir con mi compañero el codigo”);
+  printf("Mauricio Hernandez Villagran")
 return 0;
 }
